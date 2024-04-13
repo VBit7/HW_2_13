451db472db5fa6df516bd29edbc944967ffd319a
Vitalii Bielimov <vbit2000@gmail.com>
2024-04-13 20:30:00 +0300

    added docker-compose.yml

--- src/config.py
+++ src/config.py
@@ -1,10 +1,14 @@
 from typing import Any
-
 from pydantic import ConfigDict, field_validator, EmailStr
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
+    PG_DB: str = 'base'
+    PG_USER: str = 'user'
+    PG_PASSWORD: str = 'password'
+    PG_PORT: int = 5432
+    PG_DOMAIN: str = 'host'
     DB_URL: str = "postgresql+asyncpg://user:password@localhost:5432/base"
     SECRET_KEY_JWT: str = "1234567890"
     ALGORITHM: str = "HS256"
@@ -27,33 +31,7 @@ class Settings(BaseSettings):
             raise ValueError("algorithm must be HS256 or HS512")
         return v
 
-
     model_config = ConfigDict(extra='ignore', env_file=".env", env_file_encoding="utf-8")  # noqa
 
 
 config = Settings()
-
-
-
-# from dotenv import load_dotenv
-# import dotenv
-# import os
-#
-#
-# dotenv.load_dotenv()
-#
-# DB_USER = os.getenv('DB_USER')
-# DB_PASSWORD = os.getenv('DB_PASSWORD')
-# DB_HOST = os.getenv('DB_HOST')
-# DB_NAME = os.getenv('DB_NAME')
-# DB_PORT = os.getenv('DB_PORT')
-#
-# SECRET_KEY = os.getenv('SECRET_KEY')
-# ALGORITHM = os.getenv('ALGORITHM')
-#
-#
-# class Config:
-#     DB_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-
-
-# config = Config
